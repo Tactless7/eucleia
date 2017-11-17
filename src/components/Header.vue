88e28d8fba0c8c15b7afc7f213c9f0ea867dec73
Juliane Blier <blier.juliane@gmail.com>
2017-11-17 16:23:11 +0100

    HEader structure

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,19 @@
 <template>
-  <h1>Hello guys</h1>
-  
+  <div class="ec-header">
+    <ul class="ec-header-navbar">
+      <li>Home</li>
+      <li>A propos</li>
+      <li>CV</li>
+      <li>Projets</li>
+      <li>Contact</li>
+    </ul>
+
+    <div class="ec-header-title">
+      <h1>Juliane Blier</h1>
+      <h2>Développeuse Web</h2>
+      <p class="ec-header-subtitle">Web maker, Passionnée de code et Saltimbanque du travail en équipe.</p>
+    </div>
+  </div>
 </template>
 
 <script>
@@ -10,5 +23,34 @@ export default {
 </script>
 
 <style lang="stylus" scoped>
-
+  @import '../colors.styl'
+  
+  .ec-header
+    &:after
+      display block
+      content ""
+      width 80%
+      height 0.05em
+      margin 2em auto
+      background $darkBlue
+    .ec-header-navbar
+      display flex
+      justify-content flex-end
+      list-style-type none
+      > li
+        margin 1em
+      :last-of-type
+        margin-right 3em
+    
+    .ec-header-title
+      margin-top 1.5em
+      text-align center
+      h1
+        font-size 2.3em
+      h2
+        font-size 1.8em
+        margin-bottom 0.5em
+      .ec-header-subtitle
+        font-style italic
+        font-size 1.2em
 </style>
