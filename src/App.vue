c2477b2b3dfdd1f992dc152335efef006b480e19
Juliane Blier <blier.juliane@gmail.com>
2017-11-17 15:52:42 +0100

    Init working space

--- src/App.vue
+++ src/App.vue
@@ -15,12 +15,23 @@
   }
 </script>
 
-<style>
-  #app {
-    font-family: 'Avenir', Helvetica, Arial, sans-serif;
-    -webkit-font-smoothing: antialiased;
-    -moz-osx-font-smoothing: grayscale;
-    text-align: center;
-    color: #2c3e50;
-  }
+<style lang="stylus">
+  @import 'colors.styl'
+  *
+    margin 0
+    padding 0
+
+  *, *:before, *:after
+    box-sizing border-box
+
+  html, body, #app
+    width 100%
+    height 100%
+    border 0
+    font-size 100%
+
+  #app
+    font-family 'Avenir', Helvetica, Arial, sans-serif
+    color $darkBlue
+
 </style>
