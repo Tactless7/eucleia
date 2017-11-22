8f40eb630b9de1208bb25be17a9e024251b6d8b8
Juliane Blier <blier.juliane@gmail.com>
2017-11-22 13:37:05 +0100

    Added Header component

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="ec-header">
     <ul class="ec-header-navbar">
-      <li>Home</li>
-      <li>A propos</li>
-      <li>CV</li>
-      <li>Projets</li>
-      <li>Contact</li>
+      <li><a href="/">Home</a></li>
+      <li><a href="/about">A propos</a></li>
+      <li><a href="/cv">CV</a></li>
+      <li><a href="/projets">Projets</a></li>
+      <li><a href="/contact">Contact</a></li>
     </ul>
 
     <div class="ec-header-title">
@@ -17,12 +17,12 @@
 </template>
 
 <script>
-export default {
-  name: 'Header'
-}
+  export default {
+    name: 'Header'
+  }
 </script>
 
-<style lang="stylus" scoped>
+<style lang="stylus">
   @import '../colors.styl'
   
   .ec-header
@@ -30,7 +30,7 @@ export default {
       display block
       content ""
       width 80%
-      height 0.05em
+      height 0.1em
       margin 2em auto
       background $darkBlue
     .ec-header-navbar
@@ -39,8 +39,15 @@ export default {
       list-style-type none
       > li
         margin 1em
-      :last-of-type
-        margin-right 3em
+        padding-bottom 0.2em
+        border-bottom 0.2em solid transparent
+        &:last-of-type
+          margin-right 3em
+        &:hover
+          border-bottom 0.2em solid $orange
+        a
+          color $darkBlue
+          text-decoration none
     
     .ec-header-title
       margin-top 1.5em
