a25717b53b04fb0b1ae91b042a3640189a200ebb
Juliane Blier <blier.juliane@gmail.com>
2017-11-28 18:30:34 +0100

    Added social links

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -1,5 +1,10 @@
 <template>
   <div class="ec-footer">
+    <div class="ec-footer-social">
+      <a href="https://github.com/Tactless7"><i class="fa fa-lg fa-github" aria-hidden="true"></i></a>
+      <a href="https://fr.linkedin.com/in/juliane-blier-428b8b49"><i class="fa fa-lg fa-linkedin" aria-hidden="true"></i></a>
+      <a href="https://twitter.com/Tactless7"><i class="fa fa-lg fa-twitter" aria-hidden="true"></i></a>
+    </div>
   </div>
 </template>
 
@@ -13,9 +18,21 @@
   @import '../colors.styl'
 
   .ec-footer
+    display flex
+    justify-content flex-end
     width 100%
     height 2em
     margin-top 2em
     background $darkBlue
+    .ec-footer-social
+      display flex
+      margin-right 0.6em
+      a
+        display flex
+        align-items space-around
+        text-decoration none
+        i
+          color white
+          margin auto 0.3em
 </style>
 
