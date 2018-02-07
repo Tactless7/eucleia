811f1bb7d944d9be694c3ead5b1f814ff5fbd1cd
Juliane Blier <blier.juliane@gmail.com>
2018-02-07 17:51:05 +0100

    Updated projects with link, picto, etc)

--- src/data/projects.js
+++ src/data/projects.js
@@ -2,41 +2,51 @@ let projects = [
   {
     name: 'FACE Toulouse',
     type: 'Projet client',
-    year: 'mars 2017',
+    year: 'Mars 2017',
     description: '',
-    tools: 'PHP, Laravel, OctoberCMS, MySQL, Javascript ES6, Bootstrap',
+    tools: 'PHP, Laravel, OctoberCMS, MySQL, Javascript ES6, Bootstrap.',
+    link: 'https://github.com/Tactless7/projet-face-toulouse',
+    picto: 'fa fa-github',
     pictures: []
   },
   {
     name: 'Site E-commerce',
-    type: 'Certification de fin de formation',
-    year: 'avril 2017',
+    type: 'Certification de formation',
+    year: 'Avril 2017',
     description: '',
-    tools: 'PHP, Laravel, Javascript, Bootstrap',
+    tools: 'PHP, Laravel, Javascript, Bootstrap.',
+    link: 'https://github.com/Laure-Milian/certification-simplon',
+    picto: 'fa fa-github',
     pictures: []
   },
   {
     name: 'Duchesse France',
     type: 'Projet client',
-    year: 'décembre 2017',
-    description: '',
+    year: 'Décembre 2017',
+    description: 'Projet développé pour l\'association Duchesse France au bout de 3 mois de formation Simplon. L\'objectif était de ',
     tools: 'Node.js, Javascript ES6, Crontab, Express.js, Google SpreadSheet API.',
+    link: 'https://github.com/Mooncode29/Projet-DuchesseFrance',
+    picto: 'fa fa-github',
     pictures: []
   },
   {
     name: 'Adaptathon',
     type: 'Hackathon',
-    year: 'novembre 2016',
-    description: 'Hackathon sur le thème du handicap. Position finale 4/10.',
-    tools: 'HTML, CSS, Semantic-UI',
+    year: 'Novembre 2016',
+    description: 'Hackathon organisé par la ville de Toulouse sur le thème du handicap. Des professionnels de différents horizons étaient mélangés : profils créatifs, porteurs de projets, profils techniques, consultants. Les groupes étaient créés par affinité au thème choisi. J\'étais dans le groupe "Emploi et Handicap" pour une position finale de 4/10. (Au pied du podium :\'( )',
+    tools: 'HTML, CSS, Semantic-UI.',
+    link: 'https://github.com/MarineCO/Adaptathon-2016',
+    picto: 'fa fa-github',
     pictures: []
   },
   {
     name: 'Article Programmez.com',
     type: 'Article',
-    year: 'novembre 2016',
+    year: 'Novembre 2016',
     description: "Co-auteur d'un article sur le retour du Devfest Toulouse 2016.",
     tools: '',
+    link: 'http://www.programmez.com/actualites/devfest-toulouse-2016-retour-sur-la-premiere-edition-du-festival-de-developpeurs-toulousains-25220',
+    picto: 'fa fa-file-text-o',
     pictures: []
   }
 
