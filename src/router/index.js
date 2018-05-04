89f2a5cf7c42ac762e3d8fdbf1cdb0e701513357
Juliane Blier <blier.juliane@gmail.com>
2018-05-04 21:43:12 +0200

    Added meta info package

--- src/router/index.js
+++ src/router/index.js
@@ -1,5 +1,6 @@
 import Vue from 'vue'
 import Router from 'vue-router'
+import Meta from 'vue-meta'
 import Header from '@/components/Header'
 import Footer from '@/components/Footer'
 import Home from '@/pages/Home'
@@ -9,6 +10,7 @@ import Projects from '@/pages/Projects'
 import Apropos from '@/pages/Apropos'
 
 Vue.use(Router)
+Vue.use(Meta)
 
 export default new Router({
   mode: 'history',
